3a64bcd9b5ca8f35a22cb5f88981ba643fbab67a
MMarzoo <mmarzouk12378@gmail.com>
2024-08-21 18:30:47 +0300

    Task #3

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -38,6 +38,6 @@ class LRUCache(BaseCaching):
         '''
         Must return the value in self.cache_data linked to key
         '''
-        if key is None and key not in self.cache_data:
+        if key is not None and key in self.cache_data:
             self.cache_data.move_to_end(key, last=False)
         return self.cache_data.get(key, None)
