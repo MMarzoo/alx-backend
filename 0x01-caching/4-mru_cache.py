56afd55281244ea1ea5f4d7194ee5e93b3788984
MMarzoo <mmarzouk12378@gmail.com>
2024-08-21 18:46:30 +0300

    Task #4

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -29,7 +29,7 @@ class MRUCache(BaseCaching):
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
                 mru_key, _ = self.cache_data.popitem(False)
-                print(f"DISCARD: {mru_key}")
+                print("DISCARD:", mru_key)
             self.cache_data[key] = item
             self.cache_data.move_to_end(key, last=False)
         else:
@@ -39,6 +39,6 @@ class MRUCache(BaseCaching):
         '''
         Must return the value in self.cache_data linked to key.
         '''
-        if key is not None and key not in self.cache_data:
+        if key is not None and key in self.cache_data:
             self.cache_data.move_to_end(key, last=False)
         return self.cache_data.get(key, None)
