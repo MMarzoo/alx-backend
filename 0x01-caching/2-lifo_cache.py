c9225c39d2a83295d2d01b42baddde3a379b515f
MMarzoo <mmarzouk12378@gmail.com>
2024-08-21 17:46:29 +0300

    Task #2

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''
+LIFO Caching
+'''
+
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    ''' inherits from BaseCaching and is a caching system '''
+    def __init__(self):
+        ''' Initializes the cache '''
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        '''
+        assign to the dictionary self.cache_data
+        the item value for the key key
+        '''
+        if key is None and item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            if self.last_key:
+                self.cache_data.pop(self.last_key)
+                print(f'DISCARD: {self.last_key}')
+        self.last_key = key
+
+    def get(self, key):
+        '''
+        Must return the value in self.cache_data linked to key
+        '''
+        return self.cache_data.get(key, None)
