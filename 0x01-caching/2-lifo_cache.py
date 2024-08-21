87fd3ae955fe29d9c0ae192836629e4ef81ac7f4
MMarzoo <mmarzouk12378@gmail.com>
2024-08-21 17:57:15 +0300

    Task #2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -23,7 +23,7 @@ class LIFOCache(BaseCaching):
             return
         self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key, _ = self.cache_data.popitem(False)
+            first_key, _ = self.cache_data.pop(False)
             print("DISCARD:", first_key)
 
     def get(self, key):
