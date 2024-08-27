484caa7320938efcbeaf0dda9eaf75318515bc19
MMarzoo <mmarzouk12378@gmail.com>
2024-08-28 01:26:41 +0300

    Task #5

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""
+A Basic flask application
+"""
+from flask import Flask
+from flask import render_template
+from flask import request, g
+from flask_babel import Babel
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.url_map.strict_slashes = False
+
+
+class Config():
+    """
+    Application configuration class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Gets locale from request object
+    """
+    locale = request.args.get('locale', '').strip()
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user() -> dict:
+    """ Get user from login_as parameter """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request():
+    """ Set user before each request """
+    g.user = get_user()
+
+
+@app.route('/')
+def index():
+    """
+    Return the index page
+    """
+    return render_template('5-index.html', locale=get_locale(), user=g.user)
+
+
+if __name__ == "__main__":
+    app.run()
