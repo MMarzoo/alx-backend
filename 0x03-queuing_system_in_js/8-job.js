7b175d6908de55fdd7936cd97046c30c320d9dcc
MMarzoo <mmarzouk12378@gmail.com>
2024-09-25 00:55:20 +0300

    10. Writing the job creation function

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -1,25 +1,27 @@
 function createPushNotificationsJobs(jobs, queue) {
   if (!(jobs instanceof Array)) {
-    throw new Error('jobs is not an array');
+    throw new Error('Jobs is not an array');
   }
 
   jobs.forEach((jobInfo) => {
     const job = queue.create('push_notification_code_3', jobInfo);
+
+    job.save((err) => {
+      if (!err) {
+        console.log(`Notification job created: ${job.id}`);
+      }
+    });
 
     job.on('complete', () => {
       console.log(`Notification job ${job.id} completed`);
     });
 
     job.on('failed', (err) => {
-      console.log(`Notification job ${job.id} failed: ${err.message || err.toString()}`);
+      console.log(`Notification job ${job.id} failed: ${err}`);
     });
 
     job.on('progress', (progress) => {
       console.log(`Notification job ${job.id} ${progress}% complete`);
-    });
-
-    job.save(() => {
-      console.log(`Notification job created: ${job.id}`);
     });
   });
 }
