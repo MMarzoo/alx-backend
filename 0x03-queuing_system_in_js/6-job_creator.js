57c6967ca13569c4fad0cea389ecf745bebc69c1
MMarzoo <mmarzouk12378@gmail.com>
2024-09-24 23:29:04 +0300

    6. Create the Job creator

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,22 @@
+const kue = require('kue');
+
+const queue = kue.createQueue();
+
+const JobData = {
+  phoneNumber: '1234567',
+  message: 'This is the code to verify your account',
+};
+
+const job = queue
+  .create('push_notification_code', JobData)
+  .save((err) => {
+    if (!err) console.log(`Notification job created: ${job.id}`);
+  });
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
